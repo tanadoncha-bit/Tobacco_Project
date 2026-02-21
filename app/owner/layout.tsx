fbcebf4f8dfb44d331705d3c0595942f225e0215
pawarisa-13 <pawarisa.see@kkumail.com>
2026-02-21 16:31:09 +0700

    Owner

---
+++ app/owner/layout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/Sidebar/Sidebar";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen bg-[linear-gradient(140deg,#2F4156,#567C8D)]">
+      <Sidebar />
+      <main className="flex-1 bg-gray-100 rounded-l-3xl p-8 overflow-y-auto">
+        {children}
+      </main>
+    </div>
+  );
+}
