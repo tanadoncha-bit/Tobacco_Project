9b22290276f9cfd93dae43c3bc770f00a3f1306f
tanadoncha-bit <tanadon.cha@kkumail.com>
2026-03-05 02:11:05 +0700

    feat: add nextjs project files

--- utils/db.ts
+++ utils/db.ts
@@ -6,7 +6,9 @@ const globalForPrisma = global as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient()
+  new PrismaClient({
+    log: ["error"],
+  })
 
 if (process.env.NODE_ENV !== "production")
   globalForPrisma.prisma = prisma
